8c7f4c37b42075fe8e15a44ef20c263baf89da8e
Jonathan Alarcon <joona.palarconss@gmail.com>
2024-04-23 11:06:57 -0400

    comenzando desarrollo curso midu 1.0

---
+++ proyectoMidu/holamundo/src/App.jsx
@@ -0,0 +1,19 @@
+
+import Button from './components/Boton/Button';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Hola</h1>
+      <Button text={"Hola Boton"}/>
+      <Button text={"soy el boton 2"}/>
+      <Button text={"soy el boton 3"}/>
+    </>
+  )
+}
+
+export default App
